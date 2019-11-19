a56230c15c8b934baedd10fc880cc2b96a8c5205
Davin Cuaso <davinsean@gmail.com>
2019-11-19 12:02:47 +0800

    Photo Essay
    Unfinished with mobile responsiveness for the whole page

---
+++ PhotoEssayMarawi/main.js
@@ -0,0 +1,63 @@
+$(document).ready(function () {
+    function isScrolledIntoView(elem)
+    {
+        var docViewTop = $(window).scrollTop();
+        var docViewBottom = docViewTop + $(window).height();
+    
+        var elemTop = $(elem).offset().top;
+        var elemBottom = elemTop + $(elem).height();
+    
+        return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+    }
+    
+    var item = document.getElementsByClassName("p1");
+    if (isScrolledIntoView(item)){
+        console.log('visible');
+    }
+
+    $(".scroll-triangle").click(function (e) { 
+        e.preventDefault();
+        $(".p1")[0].scrollIntoView()
+    });
+
+    var comeFromTop = true;
+    var aboveNarratives = true;
+    var secondScrollPos = $(".scroll-down-top2").offset().top - 450;
+    $(window).scroll(function (event) {
+        console.log(`secondScrollPos: ${secondScrollPos}`);
+        var scroll = $(window).scrollTop();
+        console.log(`scroll: ${scroll}`);
+        // Do something
+        if (scroll >= 232 && comeFromTop){
+            $([document.documentElement, document.body]).animate({
+                scrollTop: $(".main-text").offset().top - 70
+            }, 2000);
+            comeFromTop = false;
+            console.log('set comeFromTop to false');
+            $(".p1, .p2, .p3").addClass("animated fadeInDown delay-2s")
+
+        } else if (scroll < 232 && !comeFromTop){
+            comeFromTop = true;
+            console.log('set comeFromTop to true');
+        } else if (scroll >= secondScrollPos && aboveNarratives){
+            aboveNarratives = false;
+            $([document.documentElement, document.body]).animate({
+                scrollTop: $(".the-narratives-header").offset().top - 70
+            }, 2000);
+        } else if (scroll < secondScrollPos && !aboveNarratives){
+            aboveNarratives = true;
+            console.log('aboveNarratives set to true')
+        }
+    });
+    console.log(window.pageYOffset);
+});
+
+function openSlideMenu(){
+    document.getElementById('side-menu').style.width = '250px';
+    document.getElementById('main').style.marginLeft = '250px';
+  }
+
+  function closeSlideMenu(){
+    document.getElementById('side-menu').style.width = '0';
+    document.getElementById('main').style.marginLeft = '0';
+  }
